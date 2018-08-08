ba557e7e4cffb67accc119112bce5a3dacdbb72e
JypsyGangwal <jypsy.gangwal@infobeans.com>
2018-08-08 19:43:47 +0530

    Inheritance

---
+++ srcjava/com/Inheritance/Child.java
@@ -0,0 +1,12 @@
+package com.Inheritance;
+
+public class Child extends BankingInheritance {
+
+	public void homePage() {
+		System.out.println("This is homepage");
+	}
+
+	public void mainPage() {
+		System.out.println("this is main page");
+	}
+}
