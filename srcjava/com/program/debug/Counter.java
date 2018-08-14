cae04234e89806e8e9de943612197a1b5c3815a4
tusharnaik17 <naik2227@gmail.com>
2018-08-14 15:45:17 +0530

    Debugging sample program.

---
+++ srcjava/com/program/debug/Counter.java
@@ -0,0 +1,15 @@
+package com.program.debug;
+
+public class Counter {
+	private int result = 0;
+
+	public int getResult() {
+		return result;
+	}
+
+	public void count() {
+		for (int i = 0; i < 10; i++) {
+			result += i + 1;
+		}
+	}
+}
